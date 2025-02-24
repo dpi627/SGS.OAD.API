55e9f72788001b14a73258726a315b15fed23107
brian_li <brian.li@sgs.com>
2025-02-24 16:42:37 +0800

    feat: 新增 AdInfoController 控制器
    - 引入新的控制器類別 `AdInfoController`，位於 `SGS.OAD.API.Controllers` 命名空間。
    - 使用 `[Route]` 和 `[ApiController]` 屬性來處理 HTTP 請求。
    - 包含 `GetAdInfoAsync` 方法，處理 POST 請求以根據用戶憑證檢索廣告資訊。
    - 使用非同步調用 `AdAuthHelper.GetInfoAsync` 驗證用戶憑證並返回適當的 HTTP 響應。

---
+++ SGS.OAD.API/Controllers/AdInfoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGS.OAD.AdAuth;
+using SGS.OAD.API.Models;
+
+namespace SGS.OAD.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AdInfoController(ILogger<AdValidController> logger) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<AdInfoModel>> GetAdInfoAsync(AdValidModel model)
+    {
+        try
+        {
+            var result = await AdAuthHelper.GetInfoAsync(model.UserName, model.Password, model.Domain);
+
+            if (result == default)
+            {
+                logger.LogWarning("Authorization failed.");
+                return Unauthorized("Authorization failed.");
+            }
+
+            logger.LogInformation("Get User: {@UserInfo}", result);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while processing your request.");
+            return StatusCode(500, "An error occurred while processing your request.");
+        }
+    }
+}
