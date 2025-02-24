bb1f5b6ba3b1c1979ef649289967aec0a6d35e3b
brian_li <brian.li@sgs.com>
2025-02-24 16:42:08 +0800

    feat: 新增 AdValidModel 類別
    - 在 SGS.OAD.API.Models 命名空間中引入 AdValidModel 類別
    - 類別包含 UserName、Password 和 Domain 三個屬性
    - Domain 屬性預設值為 "APAC"
    - 在檔案頂部新增 #nullable disable 指令以禁用可為 null 的引用類型

---
+++ SGS.OAD.API/Models/AdValidModel.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+namespace SGS.OAD.API.Models;
+
+public class AdValidModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Domain { get; set; } = "APAC";
+}
