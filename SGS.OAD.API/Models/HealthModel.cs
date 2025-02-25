e119f91fd128e4034cbbd36c35ef30bea638b573
brian_li <brian.li@sgs.com>
2025-02-25 15:04:27 +0800

    feat: 新增 HealthModel 和 Checks 類別
    - 新增 `HealthModel` 類別，包含 `Status` 和 `Checks` 屬性。
    - 新增 `Checks` 類別，包含 `Name`、`Status`、`Duration` 和 `Description` 屬性。
    - 加入 `#nullable disable` 指令以禁用可為空的引用類型。

---
+++ SGS.OAD.API/Models/HealthModel.cs
@@ -0,0 +1,18 @@
+﻿#nullable disable
+
+
+namespace SGS.OAD.API.Models;
+
+public class HealthModel
+{
+    public string Status { get; set; }
+    public Checks Checks { get; set; }
+}
+
+public class Checks
+{
+    public string Name { get; set; }
+    public string Status { get; set; }
+    public string Duration { get; set; }
+    public string Description { get; set; }
+}
