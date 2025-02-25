2956180c99830ae79a253e9f813a861c3545be5a
brian_li <brian.li@sgs.com>
2025-02-25 11:31:04 +0800

    feat(controller): add EmpInfoController for employee info
    - 新增 `EmpInfoController` 類別，處理員工資訊請求。
    - 包含 `GetEmpInfoAsync` 方法，根據 ID 和 ID 類型檢索員工資訊。
    - 實作錯誤處理和日誌記錄。
    - 定義 `IdType` 列舉以表示 ID 類型。

---
+++ SGS.OAD.API/Controllers/EmpInfoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGS.OAD.HrInfo;
+
+namespace SGS.OAD.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class EmpInfoController(ILogger<EmpIdController> logger) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(Employee), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetEmpInfoAsync(string id, [FromQuery] IdType idType = IdType.AdId)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+        {
+            logger.LogWarning("Invalid Id: {Id}", id);
+            return BadRequest("Invalid Id.");
+        }
+
+        try
+        {
+            var helper = await HrInfoHelper.Create().BuildAsync();
+            var emp = idType == IdType.AdId ? await helper.GetByAdIdAsync(id) : await helper.GetByEmpIdAsync(id) ;
+
+            if (emp == default || emp.StfCode == default)
+            {
+                logger.LogWarning("Can't found EmpInfo. Id: {Id}", id);
+                return NotFound($"Can't found EmpInfo. Id: {id}");
+            }
+
+            logger.LogInformation("Get EmpInfo: {EmpInfo}", emp);
+            return Ok(emp);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred. Id: {Id}", id);
+            return StatusCode(500, "An error occurred.");
+        }
+    }
+
+    public enum IdType
+    {
+        AdId,
+        EmpId
+    }
+}
