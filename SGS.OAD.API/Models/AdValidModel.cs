6555f858db1b96118a933fd7ab0b7be5f3f7bfff
brian_li <brian.li@sgs.com>
2025-02-25 10:31:54 +0800

    feat: add validation properties to AdValidModel
    - 新增 `UserName` 和 `Password` 屬性，並使用 `[Required]` 屬性進行驗證。
    - 新增 `Domain` 屬性，預設值為 "APAC"。
    - 引入 `System.ComponentModel` 和 `System.ComponentModel.DataAnnotations` 命名空間以支援資料註解。

--- SGS.OAD.API/Models/AdValidModel.cs
+++ SGS.OAD.API/Models/AdValidModel.cs
@@ -1,10 +1,16 @@
 ﻿#nullable disable
 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace SGS.OAD.API.Models;
 
 public class AdValidModel
 {
+    [Required]
     public string UserName { get; set; }
+    [Required]
     public string Password { get; set; }
+    [DefaultValue("APAC")]
     public string Domain { get; set; } = "APAC";
 }
