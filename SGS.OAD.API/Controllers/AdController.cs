2f99bbc9fef053bfa2c84f51dcc8b401cd548233
brian_li <brian.li@sgs.com>
2025-02-26 11:25:00 +0800

    feat(controller): refactor authentication methods
    - 修改 `AdController.cs`，將方法簽名從接受單獨參數改為接受 `AdRequest` 模型。
    - 更新 HTTP POST 方法及其端點描述以更具描述性。
    - 新增 `AdRequest.cs`，定義 `AdRequest` 類別，包含必要的屬性及驗證。

--- SGS.OAD.API/Controllers/AdController.cs
+++ SGS.OAD.API/Controllers/AdController.cs
@@ -8,66 +8,66 @@ namespace SGS.OAD.API.Controllers;
 [ApiController]
 public class AdController(ILogger<AdController> logger) : ControllerBase
 {
-    [HttpGet]
-    [EndpointDescription("透過 AD 帳號與密碼進行驗證")]
+    [HttpPost("valid")]
+    [EndpointDescription("驗證 AD 帳號密碼，取得驗證結果")]
     [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(bool), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> IsValidAsync(string userName, string password, string domain = "APAC")
+    public async Task<IActionResult> IsValidAsync(AdRequest model)
     {
-        if (string.IsNullOrWhiteSpace(userName) ||
-            string.IsNullOrWhiteSpace(password) ||
-            string.IsNullOrWhiteSpace(domain))
+        if (string.IsNullOrWhiteSpace(model.UserName) ||
+            string.IsNullOrWhiteSpace(model.Password) ||
+            string.IsNullOrWhiteSpace(model.Domain))
         {
-            logger.LogWarning("Invalid parameter(s). User: {UserName}", userName);
+            logger.LogWarning("Invalid parameter(s). User: {AdAccount}", model.UserName);
             return BadRequest("Invalid parameter(s).");
         }
 
         try
         {
-            bool valid = await AdAuthHelper.IsValidAsync(userName, password, domain);
+            bool valid = await AdAuthHelper.IsValidAsync(model.UserName, model.Password, model.Domain);
 
             if (!valid)
             {
-                logger.LogWarning("Autentication failed. User: {UserName}", userName);
+                logger.LogWarning("Autentication failed. User: {AdAccount}", model.UserName);
                 return Unauthorized(valid);
             }
 
-            logger.LogInformation("Autentication passed. User: {UserName}", userName);
+            logger.LogInformation("Autentication passed. User: {AdAccount}", model.UserName);
             return Ok(valid);
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "Autentication error. User: {UserName}", userName);
+            logger.LogError(ex, "Autentication error. User: {AdAccount}", model.UserName);
             return StatusCode(500, "An error occurred.");
         }
     }
 
-    [HttpPost]
-    [EndpointDescription("透過 AD 帳號與密碼進行驗證，通過後取得部分 AD 資訊")]
+    [HttpPost("info")]
+    [EndpointDescription("驗證 AD 帳號與密碼，取得 AD 開放資訊(無個資)")]
     [ProducesResponseType(typeof(AdInfoModel), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GetAdInfoAsync(string userName, string password, string domain = "APAC")
+    public async Task<IActionResult> GetAdInfoAsync(AdRequest model)
     {
-        if (string.IsNullOrWhiteSpace(userName) ||
-            string.IsNullOrWhiteSpace(password) ||
-            string.IsNullOrWhiteSpace(domain))
+        if (string.IsNullOrWhiteSpace(model.UserName) ||
+            string.IsNullOrWhiteSpace(model.Password) ||
+            string.IsNullOrWhiteSpace(model.Domain))
         {
-            logger.LogWarning("Invalid parameter(s). User: {UserName}", userName);
+            logger.LogWarning("Invalid parameter(s). User: {AdAccount}", model.UserName);
             return BadRequest("Invalid parameter(s).");
         }
 
         try
         {
-            var adInfo = await AdAuthHelper.GetInfoAsync(userName, password, domain);
+            var adInfo = await AdAuthHelper.GetInfoAsync(model.UserName, model.Password, model.Domain);
 
             if (adInfo == default)
             {
-                logger.LogWarning("Can't find AdInfo. User: {UserName}", userName);
-                return Unauthorized($"Can't find AdInfo. User: {userName}");
+                logger.LogWarning("Can't find AdInfo. User: {AdAccount}", model.UserName);
+                return Unauthorized($"Can't find AdInfo. User: {model.UserName}");
             }
 
             logger.LogInformation("Found AdInfo: {@AdInfo}", adInfo);
@@ -75,7 +75,7 @@ public class AdController(ILogger<AdController> logger) : ControllerBase
         }
         catch (Exception ex)
         {
-            logger.LogError(ex, "Error occurred. User: {UserName}", userName);
+            logger.LogError(ex, "Error occurred. User: {AdAccount}", model.UserName);
             return StatusCode(500, "Error occurred.");
         }
     }
---
+++ SGS.OAD.API/Models/AdRequest.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SGS.OAD.API.Models;
+
+public class AdRequest
+{
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    [DefaultValue("APAC")]
+    public string Domain { get; set; } = "APAC";
+}
